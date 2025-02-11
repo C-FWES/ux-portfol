5932fe0525e83fbca5d664b20be5533098e91570
C-FWES <353861933@educ.dpcdsb.org>
2025-02-10 23:10:21 -0500

    align navigation elements

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 function Navigation() {
     return (
         <div className="navigationContainer">
-            <p className="navigationElement">WESLEY OU, </p>
-            <p className="navigationElement">ABOUT, </p>
+            <p className="navigationElement">WESLEY OU, &nbsp; </p>
+            <p className="navigationElement">ABOUT, &nbsp; </p>
             <p className="navigationElement">CONTACT</p>
         </div>
     )
