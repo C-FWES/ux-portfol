2a003c32059be9a3622f1afd076a9aa95f745c15
C-FWES <353861933@educ.dpcdsb.org>
2025-02-10 23:56:22 -0500

    set up router routes in navigation

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,11 +1,27 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Navigation() {
+
+    const navigate = useNavigate();
+
+    const goToHome = () => {
+        navigate("/");
+    }
+
+    const goToAbout = () => {
+        navigate("/");
+    }
+
+    const goToContact = () => {
+        navigate("/contact");
+    }
+
     return (
         <div className="navigationContainer">
-            <p className="navigationElement">WESLEY OU, &nbsp; </p>
-            <p className="navigationElement">ABOUT, &nbsp; </p>
-            <p className="navigationElement">CONTACT</p>
+            <p className="navigationElement" onClick={goToHome}>WESLEY OU, &nbsp; </p>
+            <p className="navigationElement" onClick={goToAbout}>ABOUT, &nbsp; </p>
+            <p className="navigationElement" onClick={goToContact}>CONTACT</p>
         </div>
     )
 }
