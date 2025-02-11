ee3b1ee7f3a5a79ae8e68f7161a30c803b90a715
C-FWES <353861933@educ.dpcdsb.org>
2025-02-10 23:06:59 -0500

    create navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
+import Navigation from './Navigation'
+
 import './App.css'
 
 function App() {
@@ -8,7 +11,7 @@ function App() {
 
   return (
     <div>
-      
+      <Navigation></Navigation>
     </div>
   )
 }
---
+++ src/Navigation.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function Navigation() {
+    return (
+        <div className="navigationContainer">
+            <p className="navigationElement">WESLEY OU, </p>
+            <p className="navigationElement">ABOUT, </p>
+            <p className="navigationElement">CONTACT</p>
+        </div>
+    )
+}
+
+export default Navigation
