7f98a1e4d042255e87927b354d95668aa5d930cc
C-FWES <353861933@educ.dpcdsb.org>
2025-02-12 18:29:49 -0500

    add contact urls

--- src/ContactList.tsx
+++ src/ContactList.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 function ContactList() {
     return (
         <div className="projectListContainer">
-            <p className="projectListElement">W24OU@UWATERLOO.CA</p>
-            <p className="projectListElement">LINKEDIN</p>
-            <p className="projectListElement">GITHUB</p>
+            <a href="mailto:w24ou@uwaterloo.ca"><p className="projectListElement">W24OU@UWATERLOO.CA</p></a>
+            <a href=""><p className="projectListElement">RESUME</p></a>
+            <a href="https://www.linkedin.com/in/wesleyou/" target="blank"><p className="projectListElement">LINKEDIN</p></a>
+            <a href="https://github.com/C-FWES" target="blank"><p className="projectListElement">GITHUB</p></a>
         </div>
     )
 }
