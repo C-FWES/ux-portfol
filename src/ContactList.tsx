76d06d8d9decdc245b4762ce59907d243aceaefc
C-FWES <353861933@educ.dpcdsb.org>
2025-02-12 20:19:20 -0500

    add resume and resume link

--- src/ContactList.tsx
+++ src/ContactList.tsx
@@ -1,9 +1,10 @@
+import resume from './assets/resume_design.pdf'
 
 function ContactList() {
     return (
         <div className="projectListContainer">
             <a href="mailto:w24ou@uwaterloo.ca"><p className="projectListElement">W24OU@UWATERLOO.CA</p></a>
-            <a href=""><p className="projectListElement">RESUME</p></a>
+            <a href={resume} target='blank'><p className="projectListElement">RESUME</p></a>
             <a href="https://www.linkedin.com/in/wesleyou/" target="blank"><p className="projectListElement">LINKEDIN</p></a>
             <a href="https://github.com/C-FWES" target="blank"><p className="projectListElement">GITHUB</p></a>
         </div>
